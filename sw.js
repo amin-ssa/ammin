692db88a6b1be675756d6fb61f0bd63a316de93f
amin <aymanelff114@gmail.com>
2026-06-08 05:32:32 +0200

    update website

--- sw.js
+++ sw.js
@@ -1,16 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11116143
+    "zoneId": 11116490
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
-
-self.addEventListener('install', function(event) {
-    console.log('Monetag SW installed');
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-    console.log('Monetag SW activated');
-    self.clients.claim();
-});
